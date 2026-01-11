a99fabccbdc7cb146e1a7e4d21d66fe9364d2e3d
Something in Reality <dylanphillips0000@gmail.com>
2026-01-12 09:16:36 +1100

    Feature: Health check

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-// Test comment for GitHub Pull Request
+use std::net::TcpListener;
+use zero2prod::run;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
