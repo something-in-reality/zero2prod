d8f552eb821b39e803305fd5edb4f50d34f80510
Something in Reality <dylanphillips0000@gmail.com>
2026-02-02 15:28:52 +1100

    Update: Chapters 3-5

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -64,6 +64,6 @@ export DATABASE_URL
 sqlx database create
 sqlx migrate run
 
-&>2 echo "Postgres has been migrated, ready to go!"
+>&2 echo "Postgres has been migrated, ready to go!"
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Panic if cannot read configuration
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool =
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to connect to Postgres.");
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+
+    run(listener, connection_pool)?.await
 }
